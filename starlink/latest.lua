58ca1af5de4ec8988cced8861b32080f169f7bc8
none45 <dominiksdrobisevsmomosja@gmail.com>
2026-02-11 17:43:52 +0200

    Update latest.lua

--- starlink/latest.lua
+++ starlink/latest.lua
@@ -1,3 +1,3 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/none45/main/refs/heads/main/starlink/library/main.lua"))
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/none45/main/refs/heads/main/starlink/library/ui.lua"))()
 
 warn("welcome to starlink hub")
